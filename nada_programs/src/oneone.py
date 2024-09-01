f1216de6b37873364fd1476f559dff0f00216cc4
Thompson Mina <thompsonmina7@gmail.com>
2024-09-01 15:50:01 +0100

    refactor histogram

--- nada_programs/src/oneone.py
+++ nada_programs/src/oneone.py
@@ -12,7 +12,6 @@ def nada_main():
     db1 = SecretInteger(Input(name="db1", party=party2))
     db2 = SecretInteger(Input(name="db2", party=party2))
 
-
     # recieve input as an np array?
 
     tc1 = SecretInteger(Input(name="tc1", party=party3))
@@ -30,7 +29,7 @@ def nada_main():
     b = (Integer(2), Integer(55))
     bc = [b]
 
-    r = create_histogram(ranks_suite, nada_not)
+    r = create_histogram(ranks_suite, nada_not, nada_and)
 
 
     return [Output(r[i][2], "my_output" + str(i), party1) for i in range(5)]
@@ -44,7 +43,10 @@ def get_rank(num: SecretInteger) -> SecretInteger:
 def nada_not(bool: Boolean) -> Boolean:
     return bool.if_else(Integer(1), Integer(0)) == Integer(0)
 
-def create_histogram(array: List[Tuple[SecretInteger, SecretInteger]], not_op: nada_fn) -> List[List[SecretInteger]]:
+def nada_and(bool1: Boolean, bool2: Boolean) ->  Boolean:
+    return bool1.if_else(bool2.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+
+def create_histogram(array: List[Tuple[SecretInteger, SecretInteger]], not_op: nada_fn, and_op: nada_fn) -> List[List[SecretInteger]]:
     # histo = {}
     histogram = [ [c[0], c[1], Integer(0)] for c in array]
     for i in range(5):
@@ -59,7 +61,6 @@ def create_histogram(array: List[Tuple[SecretInteger, SecretInteger]], not_op: n
     #     else:
     #         histogram[4][2] =  histogram[4][2] + Integer(1)
         
-
         rank_matches_1st_item = histogram[0][1] == array[i][1]
         update_0 = rank_matches_1st_item.if_else(histogram[0][2] + Integer(1), histogram[0][2])
         histogram[0][2] = update_0
@@ -67,32 +68,43 @@ def create_histogram(array: List[Tuple[SecretInteger, SecretInteger]], not_op: n
         # not_rank_matches_1st_item = rank_matches_1st_item.if_else(Integer(1), Integer(0)) == Integer(0)
         not_rank_matches_1st_item = not_op(rank_matches_1st_item)
 
-        rank_only_matches_2nd_item = (not_rank_matches_1st_item).if_else((histogram[1][1] == array[i][1]).if_else(Integer(1), Integer(0)) , Integer(0)) == Integer(1)
+        # rank_only_matches_2nd_item = (not_rank_matches_1st_item).if_else((histogram[1][1] == array[i][1]).if_else(Integer(1), Integer(0)) , Integer(0)) == Integer(1)
+        rank_only_matches_2nd_item = and_op(not_rank_matches_1st_item, (histogram[1][1] == array[i][1]))
         update_1 = rank_only_matches_2nd_item.if_else(histogram[1][2] + Integer(1), histogram[1][2])
         histogram[1][2] = update_1
 
         # not_rank_matches_2nd_item = (histogram[1][1] == array[i][1]).if_else(Integer(1), Integer(0)) == Integer(0)
         not_rank_matches_2nd_item = not_op(histogram[1][1] == array[i][1])
-        not_match_1st_and_2nd_item = not_rank_matches_1st_item.if_else(not_rank_matches_2nd_item.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
-        
-        rank_only_matches_3rd_item = not_match_1st_and_2nd_item.if_else((histogram[2][1] == array[i][1]).if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+
+        # not_match_1st_and_2nd_item = not_rank_matches_1st_item.if_else(not_rank_matches_2nd_item.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        not_match_1st_and_2nd_item = and_op(not_rank_matches_1st_item, not_rank_matches_2nd_item)
+    
+        # rank_only_matches_3rd_item = not_match_1st_and_2nd_item.if_else((histogram[2][1] == array[i][1]).if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        rank_only_matches_3rd_item = and_op(not_match_1st_and_2nd_item, (histogram[2][1] == array[i][1]))
         update_2 = rank_only_matches_3rd_item.if_else(histogram[2][2] + Integer(1), histogram[2][2])
         histogram[2][2] = update_2
 
 
         # not_rank_matches_3rd_item = (histogram[2][1] == array[i][1]).if_else(Integer(1), Integer(0)) == Integer(0)
         not_rank_matches_3rd_item = not_op(histogram[2][1] == array[i][1])
-        not_rank_matches_1st_2nd_and_3rd_item = not_match_1st_and_2nd_item.if_else(not_rank_matches_3rd_item.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        # not_rank_matches_1st_2nd_and_3rd_item = not_match_1st_and_2nd_item.if_else(not_rank_matches_3rd_item.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        not_rank_matches_1st_2nd_and_3rd_item = and_op(not_match_1st_and_2nd_item, not_rank_matches_3rd_item)
 
-        rank_only_matches_4th_item = not_rank_matches_1st_2nd_and_3rd_item.if_else((histogram[3][1] == array[i][1]).if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+
+        # rank_only_matches_4th_item = not_rank_matches_1st_2nd_and_3rd_item.if_else((histogram[3][1] == array[i][1]).if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        rank_only_matches_4th_item = and_op(not_rank_matches_1st_2nd_and_3rd_item,(histogram[3][1] == array[i][1]))
         update_3 = rank_only_matches_4th_item.if_else(histogram[3][2] + Integer(1), histogram[3][2])
         histogram[3][2] = update_3
 
 
-        not_rank_matches_4th_item = (histogram[3][1] == array[i][1]).if_else(Integer(1), Integer(0)) == Integer(0)
-        not_rank_matches_1st_2nd_3rd_and_4th_item = not_rank_matches_1st_2nd_and_3rd_item.if_else(not_rank_matches_4th_item.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        # not_rank_matches_4th_item = (histogram[3][1] == array[i][1]).if_else(Integer(1), Integer(0)) == Integer(0)
+        not_rank_matches_4th_item = not_op(histogram[3][1] == array[i][1])
+        # not_rank_matches_1st_2nd_3rd_and_4th_item = not_rank_matches_1st_2nd_and_3rd_item.if_else(not_rank_matches_4th_item.if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        not_rank_matches_1st_2nd_3rd_and_4th_item = and_op(not_rank_matches_1st_2nd_and_3rd_item, not_rank_matches_4th_item)
+
         
-        rank_only_matches_5th_item = not_rank_matches_1st_2nd_3rd_and_4th_item.if_else((histogram[4][1] == array[i][1]).if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        # rank_only_matches_5th_item = not_rank_matches_1st_2nd_3rd_and_4th_item.if_else((histogram[4][1] == array[i][1]).if_else(Integer(1), Integer(0)), Integer(0)) == Integer(1)
+        rank_only_matches_5th_item = and_op(not_rank_matches_1st_2nd_3rd_and_4th_item, (histogram[4][1] == array[i][1]))
         update_4 = rank_only_matches_5th_item.if_else(histogram[4][2] + Integer(1), histogram[4][2])
         histogram[4][2] = update_4
 
